9c9ba5bcb5915e4f05d18338d4d11c81a678a037
GitThisJoel <joelbacker10@gmail.com>
2022-11-22 11:50:51 +0100

    add new function CCF

---
+++ exercises/CCF.m
@@ -0,0 +1,21 @@
+function CCF(w_t, eps_t, titleStr, M, n)
+
+    if nargin < 5
+        n = 500;
+    end
+
+    if nargin < 4
+        M = 40;
+    end
+
+    if nargin < 3
+        titleStr = "";
+    end
+
+    stem(-M:M, crosscorr(w_t, eps_t, M), "b");
+    title(sprintf('Cross correlation function %s', titleStr)), xlabel('Lag')
+    hold on
+    plot(-M:M, 2 / sqrt(n) * ones(1, 2 * M + 1), '--')
+    plot(-M:M, -2 / sqrt (n) * ones(1, 2 * M + 1), '-- ')
+    hold off
+end
