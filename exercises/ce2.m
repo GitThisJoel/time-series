dcd7b38a28179ae9a3c7e396dc9bf21b58ee2677
Frida-Norlund <82037342+Frida-Norlund@users.noreply.github.com>
2022-11-25 09:29:43 +0100

    changed initial conditions in pem and estimated model for etilde

--- exercises/ce2.m
+++ exercises/ce2.m
@@ -7,13 +7,13 @@ addpath("../CourseMaterial/Code/functions");
 rng(0);
 
 %%
-
+rng(0);
 n = 500; % Number of samples
 A3 = [1 .5];
-C3 = [1 - .3 .2];
+C3 = [1 -.3 .2];
 w = sqrt (2) * randn(n + 100, 1);
 x = filter(C3, A3, w); % Create the input
-A1 = [1 - .65];
+A1 = [1 -.65];
 A2 = [1 .90 .78];
 C = 1;
 B = [0 0 0 0 .4];
@@ -65,27 +65,44 @@ ar_model_4 = estimateARMA(x, [1 1 0 0 1], [1], "AR4", 32)
 %%
 arma_model = estimateARMA(x, [1 1], [1 1 1], "ARMA(1,2)", 32);
 
-%% Pre-whiten y_t
+%% Pre-whiten y_t to create eps_t
 w_t = filter(arma_model.A, arma_model.C, x);
 w_t = w_t(length(arma_model.A):end);
 eps_t = filter(arma_model.A, arma_model.C, y);
 eps_t = eps_t(length(arma_model.A):end);
 
 %%
-M = 40; stem(-M:M, crosscorr(w_t, eps_t, M));
+M = 40; 
+figure
+stem(-M:M, crosscorr(w_t, eps_t, M));
 title(' Cross\_correlation\_function'), xlabel (' Lag ')
 hold on
 plot(-M:M, 2 / sqrt (n) * ones(1, 2 * M + 1), '--')
 plot(-M:M, -2 / sqrt (n) * ones(1, 2 * M + 1), '--')
 hold off
 
-%%
+%% Approximated model order (4,2,0), create model and find the residual of x
+% försiktig med initialvärden här, sätter vi bara 1:or så riskerar vi att
+% kanske inte få nån lösning
 d = 4; r = 2; s = 0;
-A2 = [ones(1, r)];
+A2 = [1 zeros(1,r)];
 B = [zeros(1, d) ones(1, s + 1)];
 Mi = idpoly([1], [B], [], [], [A2]);
 z = iddata(y, x);
 Mba2 = pem(z, Mi); present(Mba2)
 etilde = resid(Mba2, z);
 
+%% Plot CCF of x and e_tilde 
 CCF(x, etilde.OutputData, "(x vs. etilde)");
+
+%% analyse e_tilde
+ACFnPACFnNormplot(etilde.y,32)
+
+% e_tilde is not white and it should not be since the delay is 4, so we are
+% expecting an MA(3), which is roughly what we have.
+
+%% Lets try to model e_tilde
+etilde_ar_1 = estimateARMA(etilde.y,[1 1], [1], 'ar1\_etilde', 32)
+present(etilde_ar_1)
+
+% This model looks good :)
