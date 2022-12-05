e18390d7d0bbda2e3c4f653fa60cfe24e8e9d899
GitThisJoel <joelbacker10@gmail.com>
2022-12-05 16:37:31 +0100

    how do i kalman?

--- exercises/ce3.m
+++ exercises/ce3.m
@@ -85,48 +85,48 @@ title("lambda estimate (min = " + opt_lambda + ")")
 
 %% 2.2 Kalman filtering
 % Example of Kalman filter
-% Simulate N samples of a process to test your code .
+% Simulate N samples of a process to test your code.
 
 y = ? % Simulated data
 
 % Define the state space equations.
-A = [? ?; ? ?];
+A = [1 0; 0 0]; % ?? ar2
 Re = [.004 0; 0 0]; % State covariance matrix
-Rw = 1.25; % Observation variance
+Rw = 1.25; %          Observation variance
 
 % Set some initial values
 Rxx1 = 10 * eye(2); % Initial state variance
-xtt1 = [0 0]'; % Initial statevalues
+xtt1 = [0 0]'; %      Initial statevalues
 
 % Vectors to store values in
-Xsave = zeros(2, N); % Store d states
-ehat = zeros(1, N); % Prediction residual
-yt1 = zeros(1, N); % One step prediction
-yt2 = zeros(1, N); % Two step prediction
+Xsave = zeros(2, N); % Stored states
+ehat = zeros(1, N); %  Prediction residual
+yt1 = zeros(1, N); %   One step prediction
+yt2 = zeros(1, N); %   Two step prediction
 
 % The filter use data up to time t −1 to predict value at t,
 % then update using the prediction error. Why do we start
 % from t = 3? Why stop at N−2?
 for t = 3:N - 2
-    Ct = [? ?]; % C { t | t −1}
-    yhat(t) = ? % y { t | t −1}
-    ehat(t) = ? % e t = y t - y { t | t −1}
+    Ct = [? ?]; % C { t | t-1}
+    yhat(t) = ? % y { t | t-1}
+    ehat(t) = y(t) - yhat(t); % et = yt - y{ t | t-1 }
 
     % Update
-    Ryy = ? % Rˆ{ yy } { t | t −1}
-    Kt = ? % Kt
+    Ryy = Rxx1 + eye(2); % Rˆ{ yy } { t | t-1 }
+    Kt = ? %  Kt
     xtt = ? % x{ t | t }
     Rxx = ? % R{ xx } { t | t }
 
     % Predict the next state
-    xtt1 = ? % x{ t+1| t }
-    Rxx1 = ? % Rˆ{ xx }{ t+1 | t }
+    xtt1 = ? % x{ t+1 | t }
+    Rxx1 = A * Rxx * A' + Re; % Rˆ{ xx }{ t+1 | t }
 
     % Form 2− step prediction. Ignore this part at first.
-    Ct1 = [? ?]; % C{ t+1 | t }
-    yt1(t +1) = ? % y{ t+1 | t } = C{ t+1 | t } x{ t | t }
-    Ct2 = [? ?]; % C{ t+2 | t }
-    yt2(t +2) = ? % y{ t+2 | t } = C{ t+2 | t } x{ t | t }
+    Ct1 = [? ?]; %   C{ t+1 | t }
+    yt1(t + 1) = ? % y{ t+1 | t } = C{ t+1 | t } x{ t | t }
+    Ct2 = [? ?]; %   C{ t+2 | t }
+    yt2(t + 2) = ? % y{ t+2 | t } = C{ t+2 | t } x{ t | t }
 
     % Store the state vector
     Xsave(:, t) = xtt;
