4abcc5556dd29e1dcf6b0a2159ff418e14631799
GitThisJoel <joelbacker10@gmail.com>
2022-12-05 15:54:29 +0100

    added kalman

--- exercises/ce3.m
+++ exercises/ce3.m
@@ -26,9 +26,9 @@ title("thx")
 close all
 lambdas = [1 0.95 0.9];
 
-plot = 1;
+do_plot = 1;
 
-if plot
+if do_plot
     figure
 end
 
@@ -45,13 +45,13 @@ for i = 1:length(lambdas)
         [Aest(kk, :), yhat(kk)] = step(X, tar2(kk));
     end
 
-    if plot
+    if do_plot
         subplot(1, length(lambdas), i)
-        plot(Aest)
+        plot(Aest);
         title(sprintf("lambda = %f", lambdas(i)))
         % subplot(2, length(lambdas), i + length(lambdas))
         hold on
-        plot(thx)
+        plot(thx);
         legend("est1", "est2", "est3", "thx1", "thx2", "Location", "southeast")
     end
 
@@ -74,7 +74,7 @@ for i = 1:length(lambda_line)
         [~, yhat(kk)] = step(X, tar2(kk));
     end
 
-    ls2(i) = sum((tar2 - yhat).^2);
+    ls2(i) = sum((tar2 - yhat) .^ 2);
 end
 
 min_ind = find(ls2 <= min(ls2));
@@ -82,3 +82,52 @@ opt_lambda = lambda_line(min_ind);
 
 plot(lambda_line, ls2);
 title("lambda estimate (min = " + opt_lambda + ")")
+
+%% 2.2 Kalman filtering
+% Example of Kalman filter
+% Simulate N samples of a process to test your code .
+
+y = ? % Simulated data
+
+% Define the state space equations.
+A = [? ?; ? ?];
+Re = [.004 0; 0 0]; % State covariance matrix
+Rw = 1.25; % Observation variance
+
+% Set some initial values
+Rxx1 = 10 * eye(2); % Initial state variance
+xtt1 = [0 0]'; % Initial statevalues
+
+% Vectors to store values in
+Xsave = zeros(2, N); % Store d states
+ehat = zeros(1, N); % Prediction residual
+yt1 = zeros(1, N); % One step prediction
+yt2 = zeros(1, N); % Two step prediction
+
+% The filter use data up to time t −1 to predict value at t,
+% then update using the prediction error. Why do we start
+% from t = 3? Why stop at N−2?
+for t = 3:N - 2
+    Ct = [? ?]; % C { t | t −1}
+    yhat(t) = ? % y { t | t −1}
+    ehat(t) = ? % e t = y t - y { t | t −1}
+
+    % Update
+    Ryy = ? % Rˆ{ yy } { t | t −1}
+    Kt = ? % Kt
+    xtt = ? % x{ t | t }
+    Rxx = ? % R{ xx } { t | t }
+
+    % Predict the next state
+    xtt1 = ? % x{ t+1| t }
+    Rxx1 = ? % Rˆ{ xx }{ t+1 | t }
+
+    % Form 2− step prediction. Ignore this part at first.
+    Ct1 = [? ?]; % C{ t+1 | t }
+    yt1(t +1) = ? % y{ t+1 | t } = C{ t+1 | t } x{ t | t }
+    Ct2 = [? ?]; % C{ t+2 | t }
+    yt2(t +2) = ? % y{ t+2 | t } = C{ t+2 | t } x{ t | t }
+
+    % Store the state vector
+    Xsave(:, t) = xtt;
+end
