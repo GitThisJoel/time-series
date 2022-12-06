5ed97d4a42830519eacbc56bc86a4b890057ce0c
GitThisJoel <joelbacker10@gmail.com>
2022-12-06 12:21:32 +0100

    done with 2.3

--- exercises/ce3.m
+++ exercises/ce3.m
@@ -156,30 +156,29 @@ plot(thx)
 
 disp("sum of square residuals = " + norm(ehat) .^ 2);
 
-%% 2.2 Kalman filtering
+%% 2.3 Kalman filtering
 % Example of Kalman filter
 % Simulate N samples of a process to test your code.
 
 close all
+clear
 
-extraN = 100;
-N = 1000;
-A0 = [1 -1.79 0.84];
-C0 = [1];
-e = randn(N + extraN, 1);
+rng(0);
 
 % simulate data
-% y = filter(C0, A0, e);
-% y = y(extraN + 1:end);
-% e = e(extraN + 1:end);
+extraN = 100;
+N = 10000;
+A0 = [1 -0.8 0.2];
+ee = 0.1 * randn(N + extraN, 1);
 
-N = length(tar2);
-y = tar2;
+y = filter(1, A0, ee);
+y = y(extraN + 1:end);
+ee = ee(extraN + 1:end);
 
 % Define the state space equations.
 A = [1 0; 0 1]; % ?? ar2
-Re = [.004 0; 0 0]; % State covariance matrix
-Rw = 1.25; %          Observation variance
+Re = [1e-6 0; 0 1e-6]; % State covariance matrix
+Rw = 0.1; %          Observation variance
 
 % Set some initial values
 Rxx1 = 10 * eye(2); % Initial state variance
@@ -210,18 +209,22 @@ for t = 3:N - 2
     Rxx1 = A * Rxx * A' + Re; % Rˆ{ xx }{ t+1 | t }
 
     % Form 2− step prediction. Ignore this part at first.
-    % Ct1 = [? ?]; %   C{ t+1 | t }
-    % yt1(t + 1) = ? % y{ t+1 | t } = C{ t+1 | t } x{ t | t }
-    % Ct2 = [? ?]; %   C{ t+2 | t }
-    % yt2(t + 2) = ? % y{ t+2 | t } = C{ t+2 | t } x{ t | t }
+    Ct1 = [-y(t) -y(t - 1)]; %   C{ t+1 | t }
+    yt1(t + 1) = Ct1 * xtt; % y{ t+1 | t } = C{ t+1 | t } x{ t | t }
+    Ct2 = [-yt1(t + 1) -y(t)]; %   C{ t+2 | t }
+    yt2(t + 2) = Ct2 * xtt; % y{ t+2 | t } = C{ t+2 | t } x{ t | t }
 
     % Store the state vector
     Xsave(:, t) = xtt;
 end
 
 figure
-plot(Xsave(:, 3:N - 2)')
+plot(y(end - 100 - 2:end - 2))
 hold on
-plot(thx)
+plot(yt1(end - 100 - 1:end - 1), 'g')
+plot(yt2(end - 100:end), 'r')
+hold off
+legend('y', 'k = 1', 'k = 2')
 
-disp("sum of square residuals = " + norm(ehat) .^ 2);
+err_resid = norm(ehat(end - 200:end)) .^ 2
+disp("sum pred residuals = " + err_resid)
