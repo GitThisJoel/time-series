0cbb1f6e06b696f7dd920d521d5eabc02f1ec98f
GitThisJoel <joelbacker10@gmail.com>
2022-12-12 15:44:11 +0100

    add data loading

--- project/partB.m
+++ project/partB.m
@@ -8,7 +8,9 @@ addpath("../exercises");
 load('raw_data.mat')
 
 halt_konc = raw_data(:, 1);
-[modeling_set, validation_set, test_set] = load_data(raw_data)
+[modeling_set, validation_set, test_set] = load_data(halt_konc);
 
-halt_ing_rep = (halt_raw_konc .* flow_raw_remill + halt_scav_konc .* flow_scav_remill) ./ flow_ing_rep;
+halt_ing_rep = create_input_signal();
+[inp_modeling_set, inp_validation_set, inp_test_set] = load_data(halt_ing_rep);
+
 %%
