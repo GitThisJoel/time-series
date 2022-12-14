ced23466e66c963e46314845658f5cbde083e0a8
GitThisJoel <joelbacker10@gmail.com>
2022-12-14 15:36:48 +0100

    moved load data

--- project/functions/load_data.m
+++ project/functions/load_data.m
@@ -1,4 +1,4 @@
-function [modeling_set, validation_set, test_set] = load_data(data, start, l_modelling, l_test)
+function [modeling_set, validation_set, test_set, index_validation, index_test] = load_data(data, start, l_modelling, l_test)
 
     data = data - mean(data);
 
@@ -17,4 +17,6 @@ function [modeling_set, validation_set, test_set] = load_data(data, start, l_mod
     modeling_set = data(start:start + l_modelling);
     validation_set = data(start + l_modelling + 1:start + l_modelling + 1 + l_test);
     test_set = data(round(start + length(data) * 0.47):round(start + length(data) * 0.47) + l_test);
+    index_validation = [start + l_modelling + 1 start + l_modelling + 1 + l_test];
+    index_test = [round(start + length(data) * 0.47) round(start + length(data) * 0.47) + l_test];
 end
