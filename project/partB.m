e43dc2f11304fd00434ff487abb2b9dafde62bd1
GitThisJoel <joelbacker10@gmail.com>
2022-12-15 11:13:19 +0100

    trying out different models

--- project/partB.m
+++ project/partB.m
@@ -9,19 +9,18 @@ addpath("functions");
 load('raw_data.mat')
 
 halt_konc = raw_data(:, 1);
-% halt_konc = halt_konc - mean(halt_konc);
 [modeling_set, validation_set, test_set] = load_data(halt_konc);
 
 halt_ing_rep = create_input_signal(raw_data);
 [inp_modeling_set, inp_validation_set, inp_test_set] = load_data(halt_ing_rep);
 
-noLags = 32;
-
-%% plot the (modelling) data
-close all
+noLags = 50;
 
 y = modeling_set;
 x = inp_modeling_set;
+
+%% plot the (modelling) data
+close all
 
 figure
 subplot(211)
@@ -30,3 +29,25 @@ title("Input signal")
 subplot(212)
 plot(y);
 title("Output signal")
+
+ACFnPACFnNormplot(x, noLags);
+
+%% cross correlation between input and output data
+
+close all
+plot_crosscorr(x, y, noLags);
+
+%% pre white
+close all
+
+input_model = estimateARMA(x, [1 1 1 1], [1 0 1 1 zeros(1, 7) 1], "ARMA(3,11) of input", noLags);
+% input_model = estimateARMA(x, [1 1 1 1], [1 0 1 1], "ARMA(3,3) of input", noLags);
+present(input_model)
+% ACFnPACFnNormplot(ex, noLags);
+
+S = 13; % from looking at estimated arma of x
+AS = [1 zeros(1, S - 1) -1];
+xdiff = myFilter(AS, 1, x, 2);
+ACFnPACFnNormplot(xdiff, noLags);
+input_model = estimateARMA(x, [1 1 1 1], [1 0 1 1], "ARMA(3,3) of input", noLags);
+present(input_model)
