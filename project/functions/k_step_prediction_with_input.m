0941127d693b35a8b2979c16109ecea1a2e9e6d6
Frida-Norlund <82037342+Frida-Norlund@users.noreply.github.com>
2022-12-16 09:00:35 +0100

    added two new functions

---
+++ project/functions/k_step_prediction_with_input.m
@@ -0,0 +1,68 @@
+function [yhatk, ehaty, ehatx] = k_step_prediction_with_input(input_model, model, k, x, y, noLags)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+% first predict the input:
+[Fx, Gx] = polydiv( input_model.C, input_model.A, k );
+xhatk = filter(Gx, input_model.C, x);
+
+figure
+plot(x)
+hold on
+plot(xhatk)
+
+
+ehatx = x - xhatk;
+ehatx = ehatx(10:end);
+
+figure
+acf( ehatx, noLags, 0.05, 1 );
+title( sprintf('ACF of the %i-step input prediction residual', k) )
+fprintf('This is a %i-step prediction. Ideally, the residual should be an MA(%i) process.\n', k, k-1)
+checkIfWhite( ehatx );
+pacfEst = pacf( ehatx, noLags, 0.05 );
+checkIfNormal( pacfEst(k+1:end), 'PACF' );
+
+%% predict output
+
+% Form the BJ prediction polynomials. In our notation, these are
+%   A1 = foundModel.D
+%   C1 = foundModel.C
+%   A2 = foundModel.F
+% 
+% The KA, KB, and KC polynomials are formed as:
+%   KA = conv( A1, A2 );
+%   KB = conv( A1, B );
+%   KC = conv( A2, C1 );
+
+KA = conv( model.D, model.F );
+KB = conv( model.D, model.B );
+KC = conv( model.F, model.C );
+
+% Form the ARMA prediction for y_t (note that this is not the same G
+% polynomial as we computed above (that was for x_t, this is for y_t).
+[Fy, Gy] = polydiv( model.C, model.D, k );
+
+% Compute the \hat\hat{F} and \hat\hat{G} polynomials.
+[Fhh, Ghh] = polydiv( conv(Fy, KB), KC, k );
+
+% Form the predicted output signal using the predicted input signal.
+yhatk  = filter(Fhh, 1, xhatk) + filter(Ghh, KC, x) + filter(Gy, KC, y);
+
+%%
+figure
+plot(y)
+hold on
+plot(yhatk)
+
+
+ehaty = y - yhatk;
+ehaty = ehaty(10:end);
+
+figure
+acf( ehaty, noLags, 0.05, 1 );
+title( sprintf('ACF of the %i-step output prediction residual', k) )
+checkIfWhite( ehaty );
+pacfEst = pacf( ehaty, noLags, 0.05 );
+checkIfNormal( pacfEst(k+1:end), 'PACF' );
+
+end
--- project/partC.m
+++ project/partC.m
@@ -4,10 +4,13 @@ clear
 addpath("../CourseMaterial/Code/data");
 addpath("../CourseMaterial/Code/functions");
 addpath("../exercises");
+addpath("functions");
 
 load('raw_data.mat')
 
 halt_konc = raw_data(:, 1);
-[modeling_set, validation_set, test_set] = load_data(halt_konc)
+[modeling_set, validation_set, test_set] = load_data(halt_konc);
+
+load('model_part_B.mat')
 
 %%
