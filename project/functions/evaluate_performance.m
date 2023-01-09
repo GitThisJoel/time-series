530626f72a3eba7b76ab589c29bc1d25c191eb19
GitThisJoel <joelbacker10@gmail.com>
2023-01-09 10:55:30 +0100

    add support for fewer input values

--- project/functions/evaluate_performance.m
+++ project/functions/evaluate_performance.m
@@ -1,17 +1,21 @@
 function [var_validation, var_test] = evaluate_performance(ehat, index_validation, index_test)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-validation_ehat = ehat(index_validation(1):index_validation(2));
-test_ehat = ehat(index_test(1):index_test(2));
+    %UNTITLED Summary of this function goes here
+    %   Detailed explanation goes here
 
+    validation_ehat = ehat(index_validation(1):index_validation(2));
+    ACFnPACFnNormplot(validation_ehat, 50, 0.05, 'Validation set', 0);
 
-ACFnPACFnNormplot(validation_ehat, 50, 0.05, 'Validation set',0);
-ACFnPACFnNormplot(test_ehat, 50, 0.05, 'Test set');
+    checkIfWhite(validation_ehat);
+    var_validation = var(validation_ehat);
 
-checkIfWhite(validation_ehat);
-var_validation = var(validation_ehat);
+    var_test = -1;
 
-checkIfWhite(test_ehat);
-var_test = var(test_ehat);
+    if (length(index_test) ~= 0) || (nargin > 2)
+        test_ehat = ehat(index_test(1):index_test(2));
+        ACFnPACFnNormplot(test_ehat, 50, 0.05, 'Test set');
+
+        checkIfWhite(test_ehat);
+        var_test = var(test_ehat);
+    end
 
 end
